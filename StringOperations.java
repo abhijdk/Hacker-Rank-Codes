ef5628e15bde3deb56b2035d1fdbf7cfb4ce3036
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-07-25 12:45:05 +0530

    Create StringOperations.java
    This exercise is to test your understanding of Java Strings. A sample String declaration:
    
    String myString = "Hello World!"
    The elements of a String are called characters. The number of characters in a String is called the length, and it can be retrieved with the String.length() method.
    
    Given two strings of lowercase English letters, A and B , perform the following operations:
    
    Sum the lengths of A and B.
    Determine if A is lexicographically larger than  B(i.e.: does B  come before A in the dictionary?).
    Capitalize the first letter in A and B and print them on a single line, separated by a space.
    Input Format
    
    The first line contains a string A. The second line contains another string B. The strings are comprised of only lowercase English letters.
    
    Output Format
    
    There are three lines of output:
    For the first line, sum the lengths of A and B.
    For the second line, write Yes if  is lexicographically greater than  otherwise print No instead.
    For the third line, capitalize the first letter in both  and  and print them on a single line, separated by a space.
    
    Sample Input 0
    
    hello
    java
    Sample Output 0
    
    9
    No
    Hello Java
    Explanation 0
    
    String A  is "hello" and  B is "java".
    
    A has a length of 5, and B has a length of 4; the sum of their lengths is 9.
    When sorted alphabetically/lexicographically, "hello" precedes "java"; therefore,A  is not greater than B and the answer is No.
    
    When you capitalize the first letter of both A and B and then print them separated by a space, you get "Hello Java".

---
+++ StringOperations.java
@@ -0,0 +1,29 @@
+package Demo;
+import java.util.Scanner;
+
+public class StringOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input two strings A and B
+        String A = scanner.nextLine();
+        String B = scanner.nextLine();
+        scanner.close();
+
+        // Sum the lengths of A and B
+        int sumLengths = A.length() + B.length();
+        System.out.println(sumLengths);
+
+        // Determine if A is lexicographically larger than B
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        // Capitalize the first letter in A and B and print them on a single line
+        String capitalizedA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String capitalizedB = B.substring(0, 1).toUpperCase() + B.substring(1);
+        System.out.println(capitalizedA + " " + capitalizedB);
+    }
+}
