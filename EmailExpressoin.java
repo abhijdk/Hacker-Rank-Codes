994e79b1570c8806731764904fb6d4ca80de7d40
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-08-14 23:01:50 +0530

    Create EmailExpressoin.java
    Write a java program to extract Email id present in input.txt file where Email id's are mixed with normal text data
    (store these Email id in a text file)

---
+++ EmailExpressoin.java
@@ -0,0 +1,23 @@
+
+package hacker_rank_codes;
+import java.io.*;
+import java.util.regex.*;
+
+public class EmailExpressoin {
+    public static void main(String[] args) throws Exception {
+    	Pattern p=Pattern.compile("[a-zA-Z0-9_.]+@[a-zA-Z0-9]+.[a-z]+");
+    	PrintWriter pw=new PrintWriter("C:\\Demo\\out.txt");
+    	BufferedReader br=new BufferedReader(new FileReader("C:\\Demo\\input.txt"));
+    	String line=br.readLine();
+    	while(line!=null) {
+    		Matcher m=p.matcher(line);
+    		while(m.find()) {
+    			pw.println(m.group());
+    		}
+    		line=br.readLine();    		
+    	}
+    	pw.close();
+    	br.close();
+    	System.out.println("Successfully Email Id Filter");
+    }
+}
